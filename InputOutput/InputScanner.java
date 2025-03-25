099dd68d293145ff4dd3a172ded10364863774f0
Jannu Akash <15jakash@gmail.com>
2025-03-25 16:38:48 +0530

    Input - Scanner method with 2 variables

--- InputOutput/InputScanner.java
+++ InputOutput/InputScanner.java
@@ -7,7 +7,9 @@ public class InputScanner {
         // Use Scanner to take input from console & read it
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
+        int y = scanner.nextInt();
         System.out.println("Hey " + x);
+        System.out.println("Hey " + y);
         scanner.close();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DSA_Prep
 DSA Preparation
+
+Input - Scanner
+
+nextInt() method - To accept multiple inputs in Java, we can use the nextInt() method of the Scanner class for each variable we want to receive input for. 
